433d2c10c84391c9bed411c6e3f137858671d723
Ishaan-Sandeep <122388323+Ishaan-Sandeep@users.noreply.github.com>
2023-05-27 12:39:15 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDGPlUTKzEugW8oJI5H7MBeJE1qOEWqHe4",
+    authDomain: "math-quiz-dbf00.firebaseapp.com",
+    databaseURL: "https://math-quiz-dbf00-default-rtdb.firebaseio.com",
+    projectId: "math-quiz-dbf00",
+    storageBucket: "math-quiz-dbf00.appspot.com",
+    messagingSenderId: "425044814863",
+    appId: "1:425044814863:web:22c1ff07f122b3abb0c77a"
+  };
+  
+  // Initialize Firebase
+ initializeApp(firebaseConfig);
